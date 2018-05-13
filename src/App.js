c169e2e8406a2c935d45e0b19494ad729929d981
EvgeneOskin <eoskin@crystalnix.com>
2018-05-13 16:22:36 +1200

    Add querying a contract field and changing it

--- src/App.js
+++ src/App.js
@@ -3,16 +3,145 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    const MyContract = window.web3.eth.contract([
+      {
+        "constant": false,
+        "inputs": [],
+        "name": "kill",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "newState",
+            "type": "string"
+          }
+        ],
+        "name": "setState",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+      },
+      {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "getSecret",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "getState",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "you_awesome",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]);
+
+    this.state = {
+      ContractInstance: MyContract.at('0x2331afc38b623a422d30efbfda4fa1079f606e43'),
+      contractState: '',
+    }
+  }
+  queryState = () => {
+    const { getState } = this.state.ContractInstance;
+
+    getState((err, secret) => {
+      if (err) { console.error('An error occured:::', err) }
+      console.log('This contract state:::', secret)
+    })
+  }
+  updateState = (event) => {
+    event.preventDefault();
+
+    const { setState } = this.state.ContractInstance;
+    const { contractState: newState } = this.state;
+
+    console.log('Try to change state');
+    setState(
+      newState,
+      {
+        gas: 300000,
+        from: window.web3.eth.accounts[0],
+        value: window.web3.toWei(0.01, 'ether'),
+      }, (err, value) => {
+        console.log('Contract state is changing');
+      }
+    )
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React and Ethereum</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <br />
+        <button onClick={this.queryState}>Query State</button>
+        <br />
+        <br />
+        <br />
+        <form onSubmit={this.updateState}>
+          <input
+            name="change-state"
+            placeholder="enter new state..."
+            value={this.state.contractState}
+            onChange={(e) => {this.setState({ contractState: e.target.value })}}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <br />
+        <br />
+
       </div>
     );
   }
